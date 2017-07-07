b333f48e0180ec2e89956faab8e372dbd15be234
azarevichvl <azarevichvl@mail.ru>
2017-07-07 13:04:49 +0300

    Add Page for add tasks. But doesn't work

---
+++ PracticProject/PracticProject/Controllers/Task/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using PracticProject.Models;
+
+namespace PracticProject.Controllers.Task
+{
+    public class TaskController : Controller
+    {
+        public ActionResult AddTaskPage()
+        {
+            PracticeDbContext db = new PracticeDbContext();
+            ViewBag.Languages = db.Languages;
+
+            return View();
+        }
+
+    }
+}
